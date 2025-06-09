868e8ecc4c763f1d10e9618280912a205b4905bd
Maq749 <enea.xhepa@students.hs-mainz.de>
2025-06-09 16:53:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/disciteomneslearningplatform/ui/settings/SettingsViewModel.java
@@ -0,0 +1,16 @@
+package com.example.disciteomneslearningplatform.ui.settings;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SettingsViewModel extends ViewModel {
+    private final MutableLiveData<String> mText;
+
+    public SettingsViewModel(){
+        mText = new MutableLiveData<>();
+        mText.setValue("Settings page");
+    }
+    public LiveData<String> getText(){return mText;}
+
+}
